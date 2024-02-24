2a71a1415ac12e13496140b7c4e8b74ac9896132
Vinay Kudari <vinay@braveorbit.io>
2024-02-24 13:17:38 -0800

    fix: typo

--- app/core/models/openai.py
+++ app/core/models/openai.py
@@ -15,7 +15,7 @@ class OpenAIModel(BaseModel):
     def load(self, model_type=M.GPT3_5):
         openai_api_key = os.getenv("OPENAI_API_KEY")
         if not openai_api_key:
-            raise ValueError("No API key set. Please set the OPEN_AI_API_KEY env var")
+            raise ValueError("No API key set. Please set the OPENAI_API_KEY env var")
 
         if model_type == M.GPT3_5:
             self.llm = ChatOpenAI(model_name=M.GPT3_5['name'], openai_api_key=openai_api_key)
