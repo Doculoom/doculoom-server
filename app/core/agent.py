e3e37af6d56c6bfad609eaeaf0da3332d17ae425
Vinay Kudari <vinay@braveorbit.io>
2024-12-18 20:08:10 -0800

    refactor code

--- app/core/agent.py
+++ app/core/agent.py
@@ -115,9 +115,9 @@ class Agent:
             index_path = self._get_index_path(name)
             if os.path.exists(index_path):
                 self.loaded_doc = name
-                self.db = FAISS.load_local(index_path, self.embeddings)
+                self.db = FAISS.load_local(index_path, self.embeddings, allow_dangerous_deserialization=True)
             else:
-                self.db = FAISS.from_texts([""], self.embeddings)
+                self.db = FAISS.from_texts([""], self.embeddings, allow_dangerous_deserialization=True)
 
             self.chain = self.create_chain(self.db)
 
